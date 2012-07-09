48dc5dc674c08268a55ba4de8dc88dd11b731f16
mpwalsh8 <mpwalsh8@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-09 18:38:57 +0000

    Moved debug code to separate file.  Moved debug content into a DIV which can be toggled on and off.
    git-svn-id: http://plugins.svn.wordpress.org/wpgform/trunk@569591 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpgform-debug.php
@@ -0,0 +1,151 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+/**
+ * GForm functions.
+ *
+ * $Id$
+ *
+ * (c) 2011 by Mike Walsh
+ *
+ * @author Mike Walsh <mike@walshcrew.com>
+ * @package wpGForm
+ * @subpackage functions
+ * @version $Revision$
+ * @lastmodified $Date$
+ * @lastmodifiedby $Author$
+ *
+ */
+
+global $wpgform_debug_content ;
+
+$wpgform_debug_content = '' ;
+add_action('init', 'wpgform_debug', 0) ;
+add_action('wp_footer', 'wpgform_show_debug_content') ;
+
+/**
+ * Debug action to examine server variables
+ *
+ */
+function wpgform_debug()
+{
+    global $wp_filter ;
+
+    if (!is_admin())
+    {
+        //error_log(sprintf('%s::%s', basename(__FILE__), __LINE__)) ;
+        wpgform_whereami(__FILE__, __LINE__) ;
+        wpgform_preprint_r($_SERVER) ;
+        wpgform_whereami(__FILE__, __LINE__) ;
+        wpgform_preprint_r($_ENV) ;
+        wpgform_whereami(__FILE__, __LINE__) ;
+        wpgform_preprint_r($_POST) ;
+        wpgform_whereami(__FILE__, __LINE__) ;
+        wpgform_preprint_r($_GET) ;
+        return;
+        if (array_key_exists('init', $wp_filter))
+        {
+            wpgform_whereami(__FILE__, __LINE__) ;
+            wpgform_preprint_r($wp_filter['init']) ;
+        }
+        if (array_key_exists('template_redirect', $wp_filter))
+        {
+            wpgform_whereami(__FILE__, __LINE__) ;
+            wpgform_preprint_r($wp_filter['template_redirect']) ;
+        }
+    }
+}
+
+/**
+ * Debug action to display debug content in a DIV which can be toggled open and closed.
+ *
+ */
+function wpgform_show_debug_content()
+{
+    global $wpgform_debug_content ;
+?>
+<style>
+h2.gform-debug {
+    text-align: center;
+    background-color: #ffebe8;
+    border: 2px solid #ff0000;
+}
+
+div.gform-debug {
+    padding: 10px;
+}
+
+div.gform-debug h2 {
+    background-color: #f00;
+}
+
+div.gform-debug h3 {
+    padding: 10px;
+    color: #ffffff;
+    font-weight: bold;
+    border: 1px solid #000000;
+    background-color: #024593;
+}
+
+div.gform-debug pre {
+    text-align: left;
+    border: 1px solid #000000;
+    background-color: #c6dffd;
+}
+</style>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+        $("div.gform-debug").hide();
+        $("a.gform-debug-wrapper").show();
+        $("a.gform-debug-wrapper").text("Show wpGForm Debug Content");
+ 
+    $("a.gform-debug-wrapper").click(function(){
+    $("div.gform-debug").slideToggle();
+
+    if ($("a.gform-debug-wrapper").text() == "Show wpGForm Debug Content")
+        $("a.gform-debug-wrapper").text("Hide wpGForm Debug Content");
+    else
+        $("a.gform-debug-wrapper").text("Show wpGForm Debug Content");
+    });
+});
+</script>
+<div class="gform-debug">
+    <?php echo $wpgform_debug_content ; ?>
+</div>
+<?php
+}
+
+/**
+ * wpgform_send_headers()
+ *
+ * @return null
+ */
+function wpgform_send_headers()
+{
+    header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+    header('Expires: ' . date(DATE_RFC822, strtotime('yesterday'))); // Date in the past
+}
+
+/**
+ * Debug "where am i" function
+ */
+function wpgform_whereami($f, $l)
+{
+    global $wpgform_debug_content ;
+
+    $wpgform_debug_content .= sprintf('<h3>%s::%s</h3>', basename($f), $l) ;
+}
+
+/**
+ * Debug functions
+ */
+function wpgform_preprint_r()
+{
+    global $wpgform_debug_content ;
+
+    $numargs = func_num_args() ;
+    $arg_list = func_get_args() ;
+    for ($i = 0; $i < $numargs; $i++) {
+	    $wpgform_debug_content .= sprintf('<pre style="text-align:left;">%s</pre>', print_r($arg_list[$i], true)) ;
+    }
+}
+?>
