f06ae1387a48fc38f399693267cf422314ec4f50
mpwalsh8 <mpwalsh8@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-26 21:21:45 +0000

    Changed all gform references to wpgform as part of migration to standard naming convention.
    git-svn-id: http://plugins.svn.wordpress.org/wpgform/trunk@704422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpgform-options.php
+++ wpgform-options.php
@@ -31,7 +31,7 @@ function wpgform_options_admin_footer()
 <!-- Setup jQuery Tabs -->
 <script type="text/javascript">
     jQuery(document).ready(function() {
-        jQuery("#gform-tabs").tabs() ;
+        jQuery("#wpgform-tabs").tabs() ;
     }) ;
 </script>
 <?php
@@ -96,22 +96,22 @@ function wpgform_options_page()
 ?>
 <br /><br />
 <div class="container">
-    <div id="gform-tabs">
+    <div id="wpgform-tabs">
         <ul>
-        <li><a href="#gform-tabs-1"><?php _e('Options', WPGFORM_I18N_DOMAIN);?></a></li>
-        <li><a href="#gform-tabs-2"><?php _e('FAQs', WPGFORM_I18N_DOMAIN);?></a></li>
-        <li><a href="#gform-tabs-3"><?php _e('Usage', WPGFORM_I18N_DOMAIN);?></a></li>
-        <li><a href="#gform-tabs-4"><?php _e('Debug', WPGFORM_I18N_DOMAIN);?></a></li>
-        <li><a href="#gform-tabs-5"><?php _e('About', WPGFORM_I18N_DOMAIN);?></a></li>
+        <li><a href="#wpgform-tabs-1"><?php _e('Options', WPGFORM_I18N_DOMAIN);?></a></li>
+        <li><a href="#wpgform-tabs-2"><?php _e('FAQs', WPGFORM_I18N_DOMAIN);?></a></li>
+        <li><a href="#wpgform-tabs-3"><?php _e('Usage', WPGFORM_I18N_DOMAIN);?></a></li>
+        <li><a href="#wpgform-tabs-4"><?php _e('Debug', WPGFORM_I18N_DOMAIN);?></a></li>
+        <li><a href="#wpgform-tabs-5"><?php _e('About', WPGFORM_I18N_DOMAIN);?></a></li>
         </ul>
-        <div id="gform-tabs-1">
+        <div id="wpgform-tabs-1">
             <form method="post" action="options.php">
                 <?php settings_fields('wpgform_options') ; ?>
                 <?php wpgform_settings_input() ; ?>
                 <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
             </form>
         </div>
-        <div id="gform-tabs-2">
+        <div id="wpgform-tabs-2">
 <?php
     //
     //  Instead of duplicating the FAQ and Other Notes content in the ReadMe.txt file,
@@ -141,7 +141,7 @@ function wpgform_options_page()
     }
 ?>
         </div>
-        <div id="gform-tabs-3">
+        <div id="wpgform-tabs-3">
 <?php
 
     if (is_wp_error($readme))
@@ -156,14 +156,14 @@ function wpgform_options_page()
     }
 ?>
         </div>
-        <div id="gform-tabs-4">
+        <div id="wpgform-tabs-4">
             <form method="post" action="options.php">
                 <?php settings_fields('wpgform_options') ; ?>
                 <?php wpgform_settings_debug() ; ?>
                 <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
             </form>
         </div>
-        <div id="gform-tabs-5">
+        <div id="wpgform-tabs-5">
         <h4><?php _e('About WordPress Google Form', WPGFORM_I18N_DOMAIN);?></h4>
         <p><?php _e('An easy to implement integration of a Google Form with WordPress. This plugin allows you to leverage the power of Google Docs Spreadsheets and Forms to collect data while retaining the look and feel of your WordPress based web site.  The forms can optionally be styled to better integrate with your WordPress theme.', WPGFORM_I18N_DOMAIN);?></p>
         <p>WordPress Google Form is based on the <a href="http://codex.wordpress.org/HTTP_API"><b>WordPress HTTP API</b></a> and in particular, the <a href="http://codex.wordpress.org/Function_API/wp_remote_get"><b>wp_remote_get()</b></a> and <a href="http://codex.wordpress.org/Function_API/wp_remote_post"><b>wp_remote_post()</b></a> functions for retrieving and posting the form.  WordPress Google Form also makes use of the <a href="http://codex.wordpress.org/Function_Reference/wp_kses"><b>wp_kses()</b></a> function for processing the HTML retrieved from Google and extracting the relevant parts of the form.</p><p>If you find this plugin useful, please consider <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DK4MS3AA983CC" target="_blank">making small donation towards this plugin</a> to help keep it up to date.</p>
@@ -190,53 +190,53 @@ function wpgform_settings_input()
         <tr valign="top">
             <th scope="row"><label><b><i>wpGForm</i></b> Shortcode</label></th>
             <td><fieldset>
-            <label for="gform_sc_posts">
-            <input name="wpgform_options[sc_posts]" type="checkbox" id="gform_sc_posts" value="1" <?php checked('1', $wpgform_options['sc_posts']) ; ?> />
+            <label for="wpgform_sc_posts">
+            <input name="wpgform_options[sc_posts]" type="checkbox" id="wpgform_sc_posts" value="1" <?php checked('1', $wpgform_options['sc_posts']) ; ?> />
             <?php _e('Enable shortcodes for posts and pages', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_sc_widgets">
-            <input name="wpgform_options[sc_widgets]" type="checkbox" id="gform_sc_widgets" value="1" <?php checked('1', $wpgform_options['sc_widgets']) ; ?> />
+            <label for="wpgform_sc_widgets">
+            <input name="wpgform_options[sc_widgets]" type="checkbox" id="wpgform_sc_widgets" value="1" <?php checked('1', $wpgform_options['sc_widgets']) ; ?> />
             <?php _e('Enable shortcodes in text widget', WPGFORM_I18N_DOMAIN);?></label>
             </fieldset></td>
         </tr>
         <tr valign="top">
             <th scope="row"><label><b><i>wpGForm</i></b> CSS</label></th>
             <td><fieldset>
-            <label for="gform_default_css">
-            <input name="wpgform_options[default_css]" type="checkbox" id="gform_default_css" value="1" <?php checked('1', $wpgform_options['default_css']) ; ?> />
+            <label for="wpgform_default_css">
+            <input name="wpgform_options[default_css]" type="checkbox" id="wpgform_default_css" value="1" <?php checked('1', $wpgform_options['default_css']) ; ?> />
             <?php _e('Enable default WordPress Google Form CSS', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_custom_css">
-            <input name="wpgform_options[custom_css]" type="checkbox" id="gform_custom_css" value="1" <?php checked('1', $wpgform_options['custom_css']) ; ?> />
+            <label for="wpgform_custom_css">
+            <input name="wpgform_options[custom_css]" type="checkbox" id="wpgform_custom_css" value="1" <?php checked('1', $wpgform_options['custom_css']) ; ?> />
             <?php _e('Enable custom WordPress Google Form CSS', WPGFORM_I18N_DOMAIN);?></label>
             </fieldset></td>
         </tr>
         <tr valign="top">
         <th scope="row"><label><?php printf(__('Custom %s CSS', WPGFORM_I18N_DOMAIN), 'wpGForm');?></label><br/><small><i><?php _e('Optional CSS styles to control the appearance of the Google Form.', WPGFORM_I18N_DOMAIN);?></i></small></th>
             <td>
-            <textarea class="regular-text code" name="wpgform_options[custom_css_styles]" rows="15" cols="80"  id="gform_custom_css_styles"><?php echo $wpgform_options['custom_css_styles']; ?></textarea>
+            <textarea class="regular-text code" name="wpgform_options[custom_css_styles]" rows="15" cols="80"  id="wpgform_custom_css_styles"><?php echo $wpgform_options['custom_css_styles']; ?></textarea>
             </td>
         </tr>
         <tr valign="top">
             <th scope="row"><label><?php _e('Confirmation Email Format', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_email_format">
-            <input name="wpgform_options[email_format]" type="radio" id="gform_email_format" value="<?php echo WPGFORM_EMAIL_FORMAT_HTML ;?>" <?php checked(WPGFORM_EMAIL_FORMAT_HTML, $wpgform_options['email_format']) ; ?> />
+            <label for="wpgform_email_format">
+            <input name="wpgform_options[email_format]" type="radio" id="wpgform_email_format" value="<?php echo WPGFORM_EMAIL_FORMAT_HTML ;?>" <?php checked(WPGFORM_EMAIL_FORMAT_HTML, $wpgform_options['email_format']) ; ?> />
             Send confirmation email (when used) in HTML format.</label>
             <br/>
-            <input name="wpgform_options[email_format]" type="radio" id="gform_email_format" value="<?php echo WPGFORM_EMAIL_FORMAT_PLAIN ;?>" <?php checked(WPGFORM_EMAIL_FORMAT_PLAIN, $wpgform_options['email_format']) ; ?> />
+            <input name="wpgform_options[email_format]" type="radio" id="wpgform_email_format" value="<?php echo WPGFORM_EMAIL_FORMAT_PLAIN ;?>" <?php checked(WPGFORM_EMAIL_FORMAT_PLAIN, $wpgform_options['email_format']) ; ?> />
             <?php _e('Send confirmation email (when used) in Plain Text format.', WPGFORM_I18N_DOMAIN);?></label>
             <br />
             <label for="bcc_blog_admin">
-            <input name="wpgform_options[bcc_blog_admin]" type="checkbox" id="gform_bcc_blog_admin" value="1" <?php checked('1', $wpgform_options['bcc_blog_admin']) ; ?> />
+            <input name="wpgform_options[bcc_blog_admin]" type="checkbox" id="wpgform_bcc_blog_admin" value="1" <?php checked('1', $wpgform_options['bcc_blog_admin']) ; ?> />
             <?php _e('Bcc Blog Admin on Confirmation Email (when used)', WPGFORM_I18N_DOMAIN);?></label>
             </fieldset></td>
         </tr>
         <tr valign="top">
             <th scope="row"><label><?php _e('HTTP API Timeout', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_http_api_timeout">
-            <select style="width: 150px;" name="wpgform_options[http_api_timeout]" id="gform_http_api_timeout">
+            <label for="wpgform_http_api_timeout">
+            <select style="width: 150px;" name="wpgform_options[http_api_timeout]" id="wpgform_http_api_timeout">
             <option value="5" <?php selected($wpgform_options['http_api_timeout'], 5); ?>>5 Seconds</option>
             <option value="10" <?php selected($wpgform_options['http_api_timeout'], 10); ?>>10 Seconds</option>
             <option value="15" <?php selected($wpgform_options['http_api_timeout'], 15); ?>>15 Seconds</option>
@@ -250,13 +250,30 @@ function wpgform_settings_input()
             </fieldset></td>
         </tr>
         <tr valign="top">
-            <th scope="row"><label><?php _e('Browser Check', WPGFORM_I18N_DOMAIN);?></label></th>
+            <th scope="row"><label><?php _e('Enable Form Submission Logging', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_browser_check">
+            <label for="wpgform_form_submission_log">
             <table style="padding: 0px;" border="0" cellpadding="0" cellspacing="0">
             <tr>
             <td style="padding: 5px 0px; vertical-align: top;">
-            <input name="wpgform_options[browser_check]" type="checkbox" id="gform_browser_check" value="1" <?php checked('1', $wpgform_options['browser_check']) ; ?> />
+            <input name="wpgform_options[form_submission_log]" type="checkbox" id="wpgform_form_submission_log" value="1" <?php checked('1', $wpgform_options['form_submission_log']) ; ?> />
+            </td>
+            <td style="padding: 5px;">
+            <?php _e('Log WordPress Google Form Submissions?<br/><small>Form submissions can be logged which will track a number of client related metrics upon form submission.</small>', WPGFORM_I18N_DOMAIN);?>
+            </td>
+            </tr>
+            </table>
+            </label>
+            </fieldset></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row"><label><?php _e('Browser Check', WPGFORM_I18N_DOMAIN);?></label></th>
+            <td><fieldset>
+            <label for="wpgform_browser_check">
+            <table style="padding: 0px;" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+            <td style="padding: 5px 0px; vertical-align: top;">
+            <input name="wpgform_options[browser_check]" type="checkbox" id="wpgform_browser_check" value="1" <?php checked('1', $wpgform_options['browser_check']) ; ?> />
             </td>
             <td style="padding: 5px;">
             <?php _e('Check browser compatibility?<br/><small>The WordPress Google Form plugin may not work as expected with older browser versions (e.g. IE6, IE7, etc.).</small>', WPGFORM_I18N_DOMAIN);?>
@@ -269,11 +286,11 @@ function wpgform_settings_input()
         <tr valign="top">
             <th scope="row"><label><?php _e('Donation Request', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_donation_message">
+            <label for="wpgform_donation_message">
             <table style="padding: 0px;" border="0" cellpadding="0" cellspacing="0">
             <tr>
             <td style="padding: 5px 0px; vertical-align: top;">
-            <input name="wpgform_options[donation_message]" type="checkbox" id="gform_donation_message" value="1" <?php checked('1', $wpgform_options['donation_message']) ; ?> />
+            <input name="wpgform_options[donation_message]" type="checkbox" id="wpgform_donation_message" value="1" <?php checked('1', $wpgform_options['donation_message']) ; ?> />
             </td>
             <td style="padding: 5px;">
             <?php _e('Hide the request for donation at the top of this page.<br/><small>The donation request will remain on the <b>About</b> tab.</small>', WPGFORM_I18N_DOMAIN);?>
@@ -311,11 +328,11 @@ function wpgform_settings_debug()
         <tr valign="top">
         <th scope="row"><label><?php _e('Enable Debug', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_enable_debug">
+            <label for="wpgform_enable_debug">
             <table style="padding: 0px;" border="0" cellpadding="0" cellspacing="0">
             <tr>
             <td style="padding: 5px 0px; vertical-align: top;">
-            <input name="wpgform_options[enable_debug]" type="checkbox" id="gform_enable_debug" value="1" <?php checked('1', $wpgform_options['enable_debug']) ; ?> />
+            <input name="wpgform_options[enable_debug]" type="checkbox" id="wpgform_enable_debug" value="1" <?php checked('1', $wpgform_options['enable_debug']) ; ?> />
             </td>
             <td style="padding: 5px;">
             <?php printf(__('Enabling debug will collect data during the form rendering and processing process.<p>The data is added to the page footer but hidden with a link appearing above the form which can toggle the display of the debug data.  This data is useful when trying to understand why the plugin isn\'t operating as expected.</p><p>When debugging is enabled, specific transports employed by the <a href="%s">WordPress HTTP API</a> can optionally be disabled.  While rarely required, disabling transports can be useful when the plugin is not communcating correctly with the Google Docs API.  <i>Extra care should be taken when disabling transports as other aspects of WordPress may not work correctly.</i>  The <a href="%s">WordPress Core Control</a> plugin is recommended for advanced debugging of <a href="%s">WordPress HTTP API issues.</a></p>', WPGFORM_I18N_DOMAIN), 'http://codex.wordpress.org/HTTP_API', 'http://wordpress.org/extend/plugins/core-control/', 'http://codex.wordpress.org/HTTP_API');?>
@@ -328,35 +345,35 @@ function wpgform_settings_debug()
         <tr valign="top">
             <th scope="row"><label><?php _e('WordPress HTTP API<br/>Transport Control', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_fsockopen_transport">
-            <input name="wpgform_options[fsockopen_transport]" type="checkbox" id="gform_fsockopen_transport" value="1" <?php checked('1', $wpgform_options['fsockopen_transport']) ; ?> />
+            <label for="wpgform_fsockopen_transport">
+            <input name="wpgform_options[fsockopen_transport]" type="checkbox" id="wpgform_fsockopen_transport" value="1" <?php checked('1', $wpgform_options['fsockopen_transport']) ; ?> />
             <?php _e('Disable <i><b>FSockOpen</b></i> Transport', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_streams_transport">
-            <input name="wpgform_options[streams_transport]" type="checkbox" id="gform_streams_transport" value="1" <?php checked('1', $wpgform_options['streams_transport']) ; ?> />
+            <label for="wpgform_streams_transport">
+            <input name="wpgform_options[streams_transport]" type="checkbox" id="wpgform_streams_transport" value="1" <?php checked('1', $wpgform_options['streams_transport']) ; ?> />
             <?php _e('Disable <i><b>Streams</b></i> Transport', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_curl_transport">
-            <input name="wpgform_options[curl_transport]" type="checkbox" id="gform_curl_transport" value="1" <?php checked('1', $wpgform_options['curl_transport']) ; ?> />
+            <label for="wpgform_curl_transport">
+            <input name="wpgform_options[curl_transport]" type="checkbox" id="wpgform_curl_transport" value="1" <?php checked('1', $wpgform_options['curl_transport']) ; ?> />
             <?php _e('Disable <i><b>cURL</b></i> Transport', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_ssl_verify">
-            <input name="wpgform_options[ssl_verify]" type="checkbox" id="gform_ssl_verify" value="1" <?php checked('1', $wpgform_options['ssl_verify']) ; ?> />
+            <label for="wpgform_ssl_verify">
+            <input name="wpgform_options[ssl_verify]" type="checkbox" id="wpgform_ssl_verify" value="1" <?php checked('1', $wpgform_options['ssl_verify']) ; ?> />
             <?php _e('Disable <i><b>SSL Verify</b></i>', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_local_ssl_verify">
-            <input name="wpgform_options[local_ssl_verify]" type="checkbox" id="gform_local_ssl_verify" value="1" <?php checked('1', $wpgform_options['local_ssl_verify']) ; ?> />
+            <label for="wpgform_local_ssl_verify">
+            <input name="wpgform_options[local_ssl_verify]" type="checkbox" id="wpgform_local_ssl_verify" value="1" <?php checked('1', $wpgform_options['local_ssl_verify']) ; ?> />
             <?php _e('Disable <i><b>Local SSL Verify</b></i>', WPGFORM_I18N_DOMAIN);?></label>
             </fieldset></td>
         </tr>
         <tr valign="top">
             <th scope="row"><label><?php _e('HTTP Request Timeout', WPGFORM_I18N_DOMAIN);?></label></th>
             <td><fieldset>
-            <label for="gform_http_request_timeout">
-            <input name="wpgform_options[http_request_timeout]" type="checkbox" id="gform_http_request_timeout" value="1" <?php checked('1', $wpgform_options['http_request_timeout']) ; ?> />
+            <label for="wpgform_http_request_timeout">
+            <input name="wpgform_options[http_request_timeout]" type="checkbox" id="wpgform_http_request_timeout" value="1" <?php checked('1', $wpgform_options['http_request_timeout']) ; ?> />
             <?php _e('Change <i><b>HTTP Request Timeout</b></i>', WPGFORM_I18N_DOMAIN);?></label>
             <br />
-            <label for="gform_http_request_timeout_value">
+            <label for="wpgform_http_request_timeout_value">
             <input name="wpgform_options[http_request_timeout_value]" type="text" id="wpgform_http_request_timeout_value" value="<?php echo $wpgform_options['http_request_timeout_value'] ; ?>" /><br />
            <small><?php _e('(in seconds)', WPGFORM_I18N_DOMAIN);?></small></label>
             </fieldset></td>
